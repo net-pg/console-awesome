5829beb094f478446c6eeb58e95b21d43edd7474
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2022-02-28 22:37:30 +0100

    #1 Add basic project with Spectre.Console

---
+++ ConsoleAwesome/ConsoleAwesome/Program.cs
@@ -0,0 +1,26 @@
+﻿using Spectre.Console;
+
+namespace ConsoleAwesome;
+
+public static partial class Program
+{
+    public static void Main(string[] args)
+    {
+        var mode = AnsiConsole.Prompt(new SelectionPrompt<Mode>()
+            .Title("Select authentication mode: ")
+            .AddChoices(
+                Mode.Anonymous,
+                Mode.Authenticated)
+            );
+
+        if (mode == Mode.Anonymous)
+        {
+            AnsiConsole.Markup("[blue]Hello Anonymous![/]");
+            return;
+        }
+
+        var name = AnsiConsole.Prompt(new TextPrompt<string>("Enter your name: "));
+
+        AnsiConsole.Markup($"[blue]Hello {name}![/]");
+    }
+}
