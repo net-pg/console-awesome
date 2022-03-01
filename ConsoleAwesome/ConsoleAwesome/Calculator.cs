13648bbc7c041a96e237aaf0d062f30d576aa3c1
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2022-03-01 17:46:44 +0100

    #3 Add helper classes

---
+++ ConsoleAwesome/ConsoleAwesome/Calculator.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleAwesome;
+
+public class Calculator
+{
+    public double Add(params double[] numbers)
+    {
+        var sum = 0d;
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+
+        return sum;
+    }
+
+    public double Subtract(double x, double y)
+    {
+        return x - y;
+    }
+
+    public double Multiply(double x, double y)
+    {
+        return x * y;
+    }
+
+    public double Divide(double x, double y)
+    {
+        if (y == 0)
+        {
+            throw new DivideByZeroException("Can not divide by 0!");
+        }
+        return x / y;
+    }
+}
---
+++ ConsoleAwesome/ConsoleAwesome/MathExpression.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleAwesome;
+
+public class MathExpression
+{
+    public string Expression { get; set; } = "0";
+    public string Output { get; set; } = "0";
+
+    public override string ToString()
+    {
+        return $"{Expression}={Output}";
+    }
+
+    public void AddOperation(char @operator, double number, string output)
+    {
+        Output = output;
+        if (@operator is '*' or '/')
+        {
+            Expression = $"({Expression}){@operator}{number.ToExpressionNumber()}";
+            return;
+        }
+        Expression = $"{Expression}{@operator}{number.ToExpressionNumber()}";
+    }
+}
